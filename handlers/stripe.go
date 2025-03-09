db85674825320caf9ca8049c983afa9c7fcf3eeb
Adam Williams <Adamleewilliams1989@gmail.com>
2025-03-09 10:37:16 -0400

    move public key to compose

--- handlers/stripe.go
+++ handlers/stripe.go
@@ -1,11 +1,12 @@
 package handlers
 
 import (
+	"os"
 	"th3-sh0p-api/restapi/operations"
 
 	"github.com/go-openapi/runtime/middleware"
 )
 
 func GetPubKey(params operations.GetPubKeyParams) middleware.Responder {
-	return operations.NewGetPubKeyOK().WithPayload("pk_test_51R0E3oRfakCijU1OfYANR05o9c37Vu5rIu9eI8pTpib0HqtJ21XxxItdSk4alLSdsN0ihg8PZ5v9Bm21nzH7DsuH00TsXibysm")
+	return operations.NewGetPubKeyOK().WithPayload(os.Getenv("STRIPE_WEBHOOK_KEY"))
 }
